1d6e1b54ded5603ccc4983979bae90742caa7288
Salman-sajib <sfsajib@gmail.com>
2024-04-29 11:50:32 +0600

    created the hook

--- src/components/UseWindowResize.jsx
+++ src/components/UseWindowResize.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 function UseWindowResize() {
   const [windowSize, setWindowSize] = useState({
@@ -6,7 +6,23 @@ function UseWindowResize() {
     height: 0,
   });
 
-  return <div>UseWindowResize</div>;
+  function handleResize() {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }
+
+  useLayoutEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
 }
 
 export default UseWindowResize;
