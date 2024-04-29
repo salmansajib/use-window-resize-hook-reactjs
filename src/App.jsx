0e10837c12e1245c86c676883fbc14845835c1c7
Salman-sajib <sfsajib@gmail.com>
2024-04-29 12:05:32 +0600

    impliment the hook to the test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Test />
     </>
   );
 }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,16 @@
+import UseWindowResize from './UseWindowResize';
+
 function Test() {
-  return <div>Test</div>;
+  const windowSize = UseWindowResize();
+  const { width, height } = windowSize;
+
+  return (
+    <div>
+      <h1>Use Window Resize Hook</h1>
+      <p> Width is {width} </p>
+      <p> Height is {height} </p>
+    </div>
+  );
 }
 
 export default Test;
